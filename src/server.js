9b682ae8b59442bec04b0b6d5cb38c3f355aef30
Lee Jeong Min <danmin20@khu.ac.kr>
2020-01-27 00:09:16 +0900

    multer middleware

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import schema from "./schema";
 import "./passport";
 import { authenticateJwt } from "./passport";
 import { isAuthenticated } from "./middlewares";
+import { uploadMiddleware, uploadController } from "./upload";
 
 const PORT = process.env.PORT || 4000;
 
@@ -16,8 +17,8 @@ const server = new GraphQLServer({
 });
 
 server.express.use(logger("dev"));
-server.express.use(authenticateJwt);
-//서버에 전달되는 모든 요청은 authenticateJwt함수를 통과함.
+server.express.use(authenticateJwt); //서버에 전달되는 모든 요청은 authenticateJwt함수를 통과함.
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 
 server.start({ port: PORT }, () =>
   console.log(`Server running on http://localhost:${PORT}`)
